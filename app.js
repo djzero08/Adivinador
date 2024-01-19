2c1ea9cd57808e78c43f5a298573dcba03548254
HECTOR MAURICIO CAMACHO RODRIGUEZ <103128379+djzero08@users.noreply.github.com>
2024-01-19 00:04:55 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+//let numeroSecreto = 6;
+let numeroUsuario = prompt("Me indicas un n�mero por favor:");
+
+console.log(numeroUsuario);
+
+if (numeroUsuario == numeroSecreto) {
+    alert('Acertaste el n�mero');
+}//
+
+
+
+
